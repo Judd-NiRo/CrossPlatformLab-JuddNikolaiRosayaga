021ec105991865440b8662b3d3a9764ecda74057
judds <judds@10.48.173.84>
2026-02-18 14:05:39 -0500

    first Commit

---
+++ src/crossplatformlab/juddnikolairosayaga/FileManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crossplatformlab.juddnikolairosayaga;
+
+/**
+ *
+ * @author judds
+ */
+public class FileManager {
+    
+    public void Printing(){
+        System.out.println("PRINTING FILESSSSSS............");
+    }
+}
